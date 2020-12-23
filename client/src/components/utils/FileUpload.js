f7b632761e361584af35a03172dcb01445c890c8
subron57 <subron57@hanmail.net>
2020-12-23 16:51:27 +0900

    fileupload Dropzone add

---
+++ client/src/components/utils/FileUpload.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Dropzone from 'react-dropzone';
+import { Icon} from 'antd'
+
+function FileUpload() {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+            <Dropzone
+                onDrop={acceptedFiles => console.log(acceptedFiles)} >
+                {({ getRootProps, getInputProps}) => (
+                    <div style={{ width: '300px', height: '240px', border:'1px solid lightgray', 
+                        display: 'flex', alignItems:'center', justifyContent:'center'}} 
+                        {...getRootProps()}>
+                        <input {...getInputProps()} />
+                        <Icon type="plus" style={{ fontSize:'3rem'}} />
+                    </div>
+                )}
+            </Dropzone>
+        </div>
+    )
+}
+
+export default FileUpload
